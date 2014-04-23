146a3900388ce8b0c8894c8f9b86b6b687eaf363
Gregory Benner <gregbenner1@gmail.com>
2014-04-23 14:03:21 -0400

    change playTone to beep and add multi-tone feature

--- Notifications.js
+++ Notifications.js
@@ -33,35 +33,52 @@ Notifications.prototype.init = function(){
 
     setTimeout(function(){
         oscillator.stop(0);
-    }, 100);
+    }, 10);
 
     return this;
 };
 
 /**
  * Play a single note
- * @param frequency in hertz
- * @param length in milliseconds
+ * @param frequency in hertz. Can be a single frequency, or can be an array of frequencies.
+ * @param totalLengthMilliseconds The total length of the entire series of tones.
+ * 
+ * If you give an array of frequencies, it will play each in succession.
  */
-Notifications.prototype.playTone = function(frequency, length){
+Notifications.prototype.beep = function(frequency, totalLengthMilliseconds){
     var parent = this;
     if(typeof this.audioContext !== 'null'){
+        if(!(frequency instanceof Array)){
+            frequency = [frequency];
+        }
+        
+        console.log(frequency);
+
+        var startTime = this.audioContext.currentTime;
+        var totalLengthSeconds = totalLengthMilliseconds / 1000;
+        var lengthOfEachBeepSeconds = totalLengthSeconds / frequency.length;
         var oscillator = this.audioContext.createOscillator();
         oscillator.start = oscillator.noteOn || oscillator.start;
         oscillator.stop = oscillator.noteOff || oscillator.stop;
         oscillator.type = this.waveType;
-        oscillator.frequency.value = frequency;
+        oscillator.frequency.value = frequency[0];
+        
+        for(var x = 1; x < frequency.length; x++){
+            console.log('change to ' + frequency[x] + ' freq in ' + lengthOfEachBeepSeconds * x + ' seconds');
+            oscillator.frequency.setValueAtTime(frequency[x], startTime + (lengthOfEachBeepSeconds * x))
+        }
+        
+        
         oscillator.connect(this.audioContext.destination);
-
         this.makingSound = true;
-        oscillator.start(0);
+        oscillator.start(startTime);
+        oscillator.stop(startTime + totalLengthSeconds);
 
         setTimeout(function(){
-            oscillator.stop(0);
             parent.makingSound = false;
-        }, length);
+        }, totalLengthMilliseconds);
     }
-
+    
     return this;
 };
 
@@ -105,7 +122,6 @@ Notifications.prototype.whenDone = function(functionToRunAfterDone){
 
     var interval = setInterval(function(){
         if(!parent.vibrating && !parent.makingSound){
-            console.log('DONE DOING STUFF!');
             functionToRunAfterDone();
             clearInterval(interval);
         }
